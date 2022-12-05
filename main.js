b1d19a62eaabd38c84cadf311d2fd74c93419135
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-12-05 11:02:25 +0400

    change values

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const data = '{"id":371,"type":"general","joke":"Why does a chicken coop only have two doors? Because if it had four doors it would be a chicken sedan."}';
 const obj = JSON.parse(data);
+obj.id = 157;
+obj.type = 'Change general';
+obj.joke = 'Joke text'
 console.log(obj)

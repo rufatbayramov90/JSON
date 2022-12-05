7bb016e5d875b390270ea996d0f7b5400d4d7248
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-12-05 10:58:47 +0400

    convert text to object

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-
+const data = '{"id":371,"type":"general","joke":"Why does a chicken coop only have two doors? Because if it had four doors it would be a chicken sedan."}';
+const obj = JSON.parse(data);
+console.log(obj)
